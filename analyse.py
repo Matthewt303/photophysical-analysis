d4dfc36f20bcfbc4c81c620c02b178e0d65dfad9
Tang <matthew.tang@stfc.ac.uk>
2025-08-01 16:06:30 +0100

    Add func to extract on/off frames

--- analyse.py
+++ analyse.py
@@ -34,6 +34,7 @@ def convert_to_photons(image: 'np.ndarray', adc: float,
 
     return image_in_photons * adc * (1 / qe) * (1 / gain)
 
+@jit(nopython=True, nogil=True, cache=False)
 def rms_calc(data: 'np.ndarray') -> float:
 
     """
@@ -85,6 +86,10 @@ def extract_spot_3d(image_stack, edges):
 @jit(nopython=True, nogil=True, cache=False)
 def extract_spot_rois(image, image_stack, spot_centers):
 
+    """
+    Not in use
+    """
+
     spots = List()
 
     for i in range(0, spot_centers.shape[0]):
@@ -118,7 +123,7 @@ def extract_spot_roi(image, image_stack, spot_center):
 
     y, x = spot_center[0], spot_center[1]
 
-    edge_coords = get_spot_edges(x, y, width=10)
+    edge_coords = get_spot_edges(x, y, width=7)
 
     if np.any(edge_coords > image.shape[0] - 1) is np.True_:
 
@@ -134,27 +139,54 @@ def extract_spot_roi(image, image_stack, spot_center):
     
     return spot
 
+def extract_on_off(spot: 'np.ndarray') -> tuple['np.ndarray', 'np.ndarray']:
+
+    threshold = rms_calc(spot)
+
+    mean_int = np.mean(np.mean(spot, axis=2), axis=1)
+
+    on_frames = np.where(mean_int > (3 * threshold))
+
+    off_frames =np.where(mean_int < (3 * threshold))
+
+    return on_frames, off_frame
+
+
 def main():
     
     file_name = ''
     
     images = load_stack(file_name)
     
-    photophysical_params = []
+    photophysical_params = list()
+
+    spot_params = np.zeros((1, 5), dtype=np.float32)
     
     for i, image in enumerate(images):
         
-        im = convert_to_photons(image)
-        threshold = rms_calc(im)
+        threshold = rms_calc(image)
         
-        filt_im = dif_of_gaussians_filt(im)
+        filt_im = dif_of_gaussians_filt(image)
         
         local_maxima = extract_local_maxima(filt_im, threshold)
+
+        frame_phphyiscal_params = list()
         
         for j, maximum in enumerate(local_maxima):
             
             spot = extract_spot_roi(image, images, maximum)
+
+            spot_no_bg = spot - np.min(spot)
             
             ### Insert code for calculations here ###
-        
+        one_frame_params = np.vstack(frame_phphyiscal_params)
+
+        frame_no = np.full(one_frame_params.shape[0], i + 1).reshape(-1, 1)
+
+        frame_params = np.hstack((frame_no, one_frame_params))
+
+        photophysical_params.append(frame_params)
+
+        del frame_params
+
         
